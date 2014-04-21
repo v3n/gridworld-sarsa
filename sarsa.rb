1a344ee8cbc911f37ea0d798349f90173f23cf40
Jonathan Howard <j@hovverd.com>
2014-04-21 01:10:58 -0400

    Implemented SARSA algorithm
    Currently untested. Stubbed out algorithm for SARSA. Still todo:
    - Action policy
    - reward_value for checking win

--- sarsa.rb
+++ sarsa.rb
@@ -1,29 +1,19 @@
-#!/usr/bin/env ruby -wKU
+#!/usr/bin/env ruby
 
 require 'CSV'
+require 'pry'
+
+Coord = Struct.new(:x, :y)
 
 class Sarsa
-  REWARD_VALUE = 1.0
+  REWARD_VALUE = 0.1
+  ENDPOINT_VALUE = 1.0
+  DISCOUT_FACTOR = 0.5
+  LEARNING_RATE = 0.5
 
-  def initialize(map_path, qtable_path)
-    @Map = 
-      if map_path.nil?
-        Array.new(20) { #fixme no starting locations
-          i = 20
-          Array.new(20) {
-            'X'
-          }
-        }
-      else
-        CSV.read map_path
-      end
-
-    @QTable = 
-      if qtable_path.nil?
-        Array.new(@Map.length) { Array.new(@Map[0].length) { 1.0 } }
-      else
-        CSV.read qtable_path
-      end
+  def initialize(map, qtable)
+    @Map = map
+    @QTable = qtable
   end
   
   def to_s
@@ -33,5 +23,34 @@ class Sarsa
   def to_csv
     @Map.map{|row| row.join(" ") }.join("\n")
   end
-  
+
+  # def [](coord)
+  #   raise TypeError, "Coordinate-like argument expected" 
+  #     unless coord.respond_to? :x and coord.respond_to? :y
+  #   @Map.[coord.y][coord.x]
+  # end
+
+  def action_policy(coord)
+    return nil
+  end
+
+  def sarsa(coord)
+    action = action_policy(coord)
+    if action.nil?
+      return REWARD_VALUE
+    end
+    
+    q_i = self.QTable[coord.y][coord.x]
+    
+    q_i + (LEARNING_RATE * (reward_value(action) + ))
+    r_1 = reward_value(action)
+    next_q = DISCOUT_FACTOR * sarsa(action)
+
+    self.QTable[coord.y][coord.x] =
+      q_i + (LEARNING_RATE * (r_1 + next_q - q_i))
+
+    # TODO: Log all qtable data here
+  end
 end
+
+binding.pry
